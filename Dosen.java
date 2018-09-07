3cefa5c68c3ed1136e5e6caf52db9ecb4125e61e
Sarah Yunita <sarahyunita111@gmail.com>
2018-09-07 17:02:09 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,21 @@
+
+public class Dosen {
+	String nama;
+	String mataKuliah;
+	
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getMataKuliah() {
+		return mataKuliah;
+	}
+	public void setMataKuliah(String mataKuliah) {
+		this.mataKuliah = mataKuliah;
+	}
+	
+	
+	
+}
