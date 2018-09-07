4dc4844db964fd5bfd6eeda20dcdf5d48244b287
Sarah Yunita <sarahyunita111@gmail.com>
2018-09-07 16:48:12 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,19 @@
+
+public class Mahasiswa {
+	String Nama;
+	String npm;
+		
+	public String getNama() {
+		return Nama;
+	}
+	public void setNama(String nama) {
+		Nama = nama;
+	}
+	public String getNpm() {
+		return npm;
+	}
+	public void setNpm(String npm) {
+		this.npm = npm;
+	}
+	
+}
